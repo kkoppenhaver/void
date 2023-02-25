553524f82dfbfc8b23c5f435713b4d1259b7b69d
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-02-25 17:05:06 -0600

    Add explainer copy and animation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { useState } from 'react';
 function App() {
   const [voidText, setVoidText] = useState('');
 
+  const [explainerTextShown, setExplainerTextShown] = useState(false);
+
   function maybeTrimText(event) {
     let textInput = event.target.value;
+
+    setExplainerTextShown(true);
 
     if( textInput.length > 40 ) {
       textInput = textInput.slice(1);
@@ -16,6 +20,12 @@ function App() {
 
   return (
     <div className="App">
+      <div className={`text-container ${voidText || explainerTextShown ? "invisible" : "visible"}`}>
+        <h1>The Void</h1>
+        <p>Type your thoughts into The Void and your text will disappear as you type.</p>
+        <p>No spell check, no save button, no judgement.</p>
+      </div>
+
       <div className="overlay"></div>
       <input autoFocus value={voidText} className="void-input" type="text" onChange={maybeTrimText} />
     </div>
