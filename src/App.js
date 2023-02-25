85f54a08eb4bfc683e9767699338d72806196dfc
Keanan Koppenhaver <keanan@alphaparticle.com>
2023-02-25 15:15:19 -0600

    First working version of the void

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [voidText, setVoidText] = useState('');
+
+  function maybeTrimText(event) {
+    let textInput = event.target.value;
+
+    if( textInput.length > 40 ) {
+      textInput = textInput.slice(1);
+    }
+
+    setVoidText(textInput); 
+  }
+
   return (
     <div className="App">
-      <div class="overlay"></div>
-      <input autoFocus class="void-input" type="text" />
+      <div className="overlay"></div>
+      <input autoFocus value={voidText} className="void-input" type="text" onChange={maybeTrimText} />
     </div>
   );
 }
